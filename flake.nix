d821588ccfaefb85ce56c798b5f15ec8d341d85a
DenHax <whatxpw@gmail.com>
2026-01-14 01:06:21 +0300

    feat: add bin2exe as shell command, nix package

--- flake.nix
+++ flake.nix
@@ -1,24 +1,54 @@
 {
-  description = "Devshell for Python";
+  description = "Devshell for Python with bin2exe development";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        bin2exe = pkgs.writeShellApplication {
+          name = "bin2exe";
+          runtimeInputs = [ pkgs.python3 ];
+          text = ''
+            exec ${pkgs.python3}/bin/python3 ${./bin/bin2exe.py} "$@"
+          '';
+        };
+
+        bin2exe-dev = pkgs.writeShellScriptBin "bin2exe-dev" ''
+          cd "${toString ./.}"
+          exec ${pkgs.python3}/bin/python3 ./bin/bin2exe.py "$@"
+        '';
       in
       {
+        packages.bin2exe = bin2exe;
+        packages.default = bin2exe;
+
+        apps.bin2exe = {
+          type = "app";
+          program = "${bin2exe}/bin/bin2exe";
+        };
+        apps.default = self.apps.${system}.bin2exe;
+
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.uv
             (pkgs.python3.withPackages (python-pkgs: [
               python-pkgs.pip
             ]))
+
+            bin2exe-dev
+            bin2exe
           ];
         };
       }
