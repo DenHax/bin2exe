572370d98a2c063092d429d4990718a2086c2c47
DenHax <whatxpw@gmail.com>
2026-01-14 00:29:17 +0300

    chore: setup devenv

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Devshell for Python";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.uv
+            (pkgs.python3.withPackages (python-pkgs: [
+              python-pkgs.pip
+            ]))
+          ];
+        };
+      }
+    );
+}
